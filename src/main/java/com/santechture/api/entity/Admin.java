ad7d4f5c214d07b5215a343538f4728af384cddb
egomaa <egomaa@ntgclarity.com>
2023-10-01 23:09:06 +0300

    first commit

---
+++ src/main/java/com/santechture/api/entity/Admin.java
@@ -0,0 +1,38 @@
+package com.santechture.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admin")
+@Entity
+public class Admin {
+
+
+    @GeneratedValue
+    @Id
+    @Column(name = "admin_id", nullable = false)
+    private Integer adminId;
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 150)
+    private String username;
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 70)
+    private String password;
+
+
+}
